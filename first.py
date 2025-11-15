6ff6d00c38beefaf80743e8616b107c1cc587dcc
Rohan8998 <pardeshirohan100@gmail.com>
2025-11-15 21:00:45 +0530

    First  Program

---
+++ first.py
@@ -0,0 +1,20 @@
+print("Hellow world")
+
+N1 = input("Hello Please Enter 1st Number: ")
+N2 = input("Hello Please Enter 2nd Number: ")
+
+Addition =int (N1) + int (N2)
+Substraction= int (N1) - int (N2)
+Multiply= int (N1) * int (N2)
+Division= int (N1) / int (N2)
+
+print("Addition : ", Addition )
+print("Substraction : ", Substraction )
+print("Multiply : ",Multiply )
+print("Division : ",Division )
+
+
+Firstname = input("Please Enetr First name: ")
+Lastname = input ("Please Emter Last name:")
+
+print(f"Hello {Firstname} {Lastname} Welcome to my first Program")
