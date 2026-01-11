7a4abce5b6759ba8fc5e0e9b4641982b8f0a58d9
Rohan8998 <pardeshirohan100@gmail.com>
2026-01-11 12:56:49 +0530

    Assignment 2

---
+++ Assignment 2.py
@@ -0,0 +1,17 @@
+n1= int(input("Please Enter an Number: "))
+if n1%2==0:
+    print(f"{n1} is the Even number")
+
+else:
+    print(f"{n1} is Odd number")
+
+
+
+
+total_sum = 0
+
+for number in range(1, 51):
+    
+   
+    total_sum += number 
+print(f"The sum of numbers from 1 to 50 is: {total_sum}")
